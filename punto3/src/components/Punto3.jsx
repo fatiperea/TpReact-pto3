31a3471c2da110863ece7b3ab71330c1a4e24cb4
fatiperea <fatimaperea3pppp0@gmail.com>
2024-02-01 12:42:09 -0300

    formato

--- punto3/src/components/Punto3.jsx
+++ punto3/src/components/Punto3.jsx
@@ -2,11 +2,14 @@ import { useState } from "react";
 
 const Punto3 = (props) => {
   const [msj, setMsj] = useState(" ");
-  
+
   return (
     <h1>
-      Hello {props.friend} {msj}<br />
-      <button onClick={()=>{setMsj(msj+"(from changed state)")}}>Click me</button>
+      Hello {props.friend} {msj}
+      <br />
+      <button onClick={() => setMsj(msj + "(from changed state)")}>
+        Click me
+      </button>
     </h1>
   );
 };
